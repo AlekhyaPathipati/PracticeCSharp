1dbe714ff3f66f70779d4dd1e28969cadbd0040a
AlekhyaPathipati <106488346+AlekhyaPathipati@users.noreply.github.com>
2022-05-30 00:23:21 +0530

    Add files via upload

---
+++ Class Datatypes.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PracticeCSharp.Assignment
+{
+    internal class Datatypes
+    {
+        static void Main(String[] args)
+        {
+            //DataTypes
+            int i = 10;
+            long l = 231708532189044;
+            float f = 5.87F;
+            double d = 77.89D;
+            String name = "Learning c#";
+            bool result = true;
+            char ch = 'a';
+            Console.WriteLine(f);
+            Console.WriteLine(d);
+            Console.WriteLine(i);
+            Console.WriteLine(l);
+            Console.WriteLine(name);
+            Console.WriteLine(ch);
+            Console.WriteLine(result);
+            //Writing and Reading from console
+            Console.WriteLine("Enter Username:");
+            String Username = Console.ReadLine();
+            Console.WriteLine("Username is: " + Username);
+            Console.ReadLine();
+        }
+    }
+}
