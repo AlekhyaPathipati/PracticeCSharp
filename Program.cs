b3ad42404c8aeaebe48ecc205dff6e506e753cdd
AlekhyaPathipati <106488346+AlekhyaPathipati@users.noreply.github.com>
2022-05-30 00:28:19 +0530

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Interface
+{
+    internal class Program
+    {
+        interface interface1
+        {
+            void show();
+        }
+        class MyClass : interface1
+        {
+            public void show()
+            {
+                Console.WriteLine("Welcome");
+            }
+            static void Main(string[] args)
+            {
+                MyClass obj1 = new MyClass();
+                obj1.show();
+                Console.ReadLine();
+
+            }
+        }
+    }
+}
