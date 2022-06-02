5c4b774b6bb5c9a99b0efe557dbd699696a666e5
AlekhyaPathipati <106488346+AlekhyaPathipati@users.noreply.github.com>
2022-06-02 17:27:25 +0530

    Add files via upload

---
+++ Arithmeticoperation/Arithmeticoperation/Program.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arithmeticoperation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            double firstNum;
+            double secondNum;
+            double answer;
+            String ops;
+
+            Console.WriteLine(".....Console Calculator....");
+            Console.WriteLine("Select an operator: (+, -, *, /)");
+            ops = Console.ReadLine();
+            do
+            {
+                
+                if ((ops == "+") || (ops == "-") || (ops == "*") || (ops == "/"))
+                {
+                    Console.WriteLine("Enter firstNum = ");
+                    firstNum = double.Parse(Console.ReadLine());
+                    Console.WriteLine("Enter secondNum = ");
+                    secondNum = double.Parse(Console.ReadLine());
+                    if (ops == "+")
+                    {
+                        answer = Addition(firstNum, secondNum);
+                        Console.WriteLine(answer);
+                    }
+                    if (ops == "-")
+                    {
+                        answer = Subtraction(firstNum, secondNum);
+                        Console.WriteLine(answer);
+                    }
+                    if (ops == "*")
+                    {
+                        answer = Multiplication(firstNum, secondNum);
+                        Console.WriteLine(answer);
+                    }
+                    if (ops == "/")
+                    {
+                        answer = Division(firstNum, secondNum);
+                        Console.WriteLine(answer);
+                    }
+                }
+                
+            }
+            while (false);
+            {
+                Console.WriteLine("invalid operator please select right one..");
+            }
+              
+        }
+
+            public static double Addition(double a, double b)
+            {
+                return a + b;
+            }
+            public static double Subtraction(double a, double b)
+            {
+                return a - b;
+            }
+            public static double Multiplication(double a, double b)
+            {
+                return a * b;
+            }
+            public static double Division(double a, double b)
+            {
+                return a / b;
+            }
+    }
+}
---
+++ Arithmeticoperation/TryCatch/TryCatch/Program.cs
@@ -0,0 +1,69 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TryCatch
+{
+     class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                double firstNum;
+                double secondNum;
+                double answer;
+                String ops;
+                Console.WriteLine(".....Console Calculator....");
+                Console.WriteLine("Enter firstNum = ");
+                firstNum = double.Parse(Console.ReadLine());
+                Console.WriteLine("Select an operator: (+, -, *, /, %)");
+                ops = Console.ReadLine();
+                Console.WriteLine("Enter secondNum = ");
+                secondNum = double.Parse(Console.ReadLine());
+
+                if (ops == "+")
+                {
+                    answer = firstNum + secondNum;
+                    Console.WriteLine(answer);
+                }
+                if (ops == "-")
+                {
+                    answer = firstNum - secondNum;
+                    Console.WriteLine(answer);
+                }
+                if (ops == "*")
+                {
+                    answer = firstNum * secondNum;
+                    Console.WriteLine(answer);
+                }
+                if (ops == "/")
+                {
+                    if(secondNum==0)
+                    {
+                        throw new Exception("secondNum can't be zero");
+                    }
+                    answer = firstNum / secondNum;
+                    Console.WriteLine(answer);
+                }
+                if (ops == "%")
+                {
+                    answer = firstNum % secondNum;
+                    Console.WriteLine(answer);
+                }
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.Message);
+
+            }
+            finally
+            {
+                Console.WriteLine("Program executed");
+            }
+
+        }
+    }
+    }
