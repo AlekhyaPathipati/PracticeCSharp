401fd5ea0dce3e66410ec0838bfbcc4a14e14934
AlekhyaPathipati <106488346+AlekhyaPathipati@users.noreply.github.com>
2022-06-06 18:23:38 +0530

    Add files via upload

---
+++ LinqProject1/LinqProject1/Class1.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProject1
+{
+     class Class1
+    {
+        static void main()
+        {
+            int[] arr = { 10, 34, 57, 90, 78, 24, 13, 89, 9 };
+            //Query 1
+            var brr = from i in arr 
+                      where i > 30 
+                      orderby i descending
+                      select i;
+            //Execute the quary
+            foreach (int x in brr)
+            {
+                Console.WriteLine(x + " ");
+            }
+        }
+    }
+}
---
+++ LinqProject1/LinqProject1/Class3.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinqProject1
+{
+     class Class3
+    {
+        //orderby
+        static void main()
+        {
+            int[] numbers = { 5, 7, 9, 13, 20, 7, 1, 10 };
+            var firstNumbers=numbers.TakeWhile(x => x >10);
+            Console.WriteLine("first numbers greater than 10");
+            foreach(int number in firstNumbers)
+            {
+                Console.WriteLine(number);
+            }
+
+
+        }
+    }
+}
